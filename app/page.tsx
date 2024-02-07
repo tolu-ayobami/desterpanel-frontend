c879649b2e5bb55950f76df5e33a1aecf97c72bb
Taiwo Jolomi <taiwojolomi15@gmail.com>
2024-02-07 12:47:02 +0100

    Task: created skeleton basic loader for dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Dashboard from "@/components/loaders/Dashboard";
+import Navbar from "@/components/loaders/Navbar";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-         <h1 className="text-3xl">Desterhost Admin Panel</h1>
-         <Dashboard />
+		<main className="">
+         <Navbar />
+			<Dashboard />
 		</main>
 	);
 }

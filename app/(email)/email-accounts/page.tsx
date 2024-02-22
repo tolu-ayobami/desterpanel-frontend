5f3d7603bf86384412b932115159e8a647254db1
Taiwo Jolomi <taiwojolomi15@gmail.com>
2024-02-22 13:46:13 +0100

    - init email route folder structure

---
+++ app/(email)/email-accounts/page.tsx
@@ -0,0 +1,12 @@
+
+const page = () => {
+  return (
+		<main className="">
+			<h1 className="text-[26px] font-bold w-full mb-[10px] lg:mb-[20px]">
+				Email Account
+			</h1>
+		</main>
+	);
+}
+
+export default page
--- app/email-accounts/page.tsx
+++
@@ -1,8 +0,0 @@
-
-const page = () => {
-  return (
-    <main>Email Account</main>
-  )
-}
-
-export default page

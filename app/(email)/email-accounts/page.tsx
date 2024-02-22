5302b57572a38b330231e4162d9c48a32c24aae4
Taiwo Jolomi <taiwojolomi15@gmail.com>
2024-02-22 17:07:15 +0100

    - creating reuseable breadcrumb component

--- app/(email)/email-accounts/page.tsx
+++ app/(email)/email-accounts/page.tsx
@@ -1,12 +1,21 @@
+import NextBreadcrumb from "@/components/common/Breadcrumbs";
 
 const page = () => {
-  return (
+	return (
 		<main className="">
 			<h1 className="text-[26px] font-bold w-full mb-[10px] lg:mb-[20px]">
 				Email Account
 			</h1>
+				<NextBreadcrumb
+					homeElement={"Home"}
+					separator={<span> &gt; </span>}
+					activeClasses="text-primary-blue"
+					containerClasses="flex py-5"
+					listClasses="hover:underline mx-2"
+					capitalizeLinks
+				/>
 		</main>
 	);
-}
+};
 
-export default page
+export default page;
